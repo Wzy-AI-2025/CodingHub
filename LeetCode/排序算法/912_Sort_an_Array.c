cf476aedf3c09f43bda53130a508c093a4ff06d1
Wzy-AI-2025 <1077714114@qq.com>
2026-05-29 20:51:25 +0800

    新增排序算法练习

---
+++ LeetCode/排序算法/912_Sort_an_Array.c
@@ -0,0 +1,8 @@
+int cmp(const void *a,const void *b){
+    return *(int*)a-*(int*)b;
+}
+int* sortArray(int* nums, int numsSize, int* returnSize) {
+    qsort(nums,numsSize,sizeof(int),cmp);
+    *returnSize=numsSize;
+    return nums;
+}
