7d4aad09e3a67d0cd56c27ed5f7fea097d496fb4
Wzy-AI-2025 <1077714114@qq.com>
2026-06-21 21:34:00 +0800

    与前面树的最大深度很相似

---
+++ LeetCode/树/543_Diameter_of_Binary_Tree.c
@@ -0,0 +1,15 @@
+int ans = 0;
+
+int depth(struct TreeNode* root) {
+    if (root == NULL) return 0;
+    int l = depth(root->left);
+    int r = depth(root->right);
+    ans = ans > l + r ? ans : l + r;
+    return (l > r ? l : r) + 1;
+}
+
+int diameterOfBinaryTree(struct TreeNode* root) {
+    ans = 0;
+    depth(root);
+    return ans;
+}
