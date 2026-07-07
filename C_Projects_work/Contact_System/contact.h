bed9bf12038c8abf15257d056d96c142ca678f33
Wzy-AI-2025 <1077714114@qq.com>
2026-07-07 23:21:54 +0800

    完善功能

--- C_Projects_work/Contact_System/contact.h
+++ C_Projects_work/Contact_System/contact.h
@@ -1,11 +1,11 @@
-#ifndef CONTACT_h//防止出现重复头文件,导致错误
+#ifndef CONTACT_h//防止头文件重复包含,避免重复定义
 #define CONTACT_h
-#define MAX_NAME 20//定义名字最大长度字节
+#define MAX_NAME 20//姓名最大长度
 #define MAX_SEX 6//性别
 #define MAX_PASS 12//电话号码
 #define MAX_ADRESS 100//住址
 
-//联系人数据
+//联系人信息结构体定义
 typedef struct People{
 char name[MAX_NAME];
 char sex[MAX_SEX];
@@ -13,23 +13,22 @@ char pass[MAX_PASS];
 char adress[MAX_ADRESS];
 }People;
 
-//链表节点结构
+//链表节点结构体定义
 typedef struct  Node{
-People date;//联系人数据
-struct  Node *next;//指针
+People date;//联系人信息
+struct  Node *next;// 指向下一个节点
 }Node;
 
-//基本工具
-void CheckName(char*,char*);//检验名字是否匹配成功
-void CreatNode(People date);//创建节点
-void DeleteNode(Node*cur);//删除节点
+//相关工具函数
+Node* CheckName(char*,Node*head);//根据姓名查找联系人,返回对应节点指针
+void CreatNode(People date,Node*head);// 创建新节点
+void DeleteNode(Node*target,Node*head);//删除对应节点
 
-//基本功能
-void Add(Node*tail);//添加联系人,用尾插法
+//功能函数声明
+void Add(Node*head);//添加联系人,使用头插法
 void Delete(Node*head);//删除联系人
-void Check(Node *head);//查找联系人
-void Change(Node *head);//修改联系人
+void Check(Node *head);//查询联系人
+void Change(Node *head);//修改联系人信息
 void Show(Node*head);//展示所有联系人
-void Clear(Node*head);//清空联系人
-
+void Clear(Node*head);//清空通讯录
 #endif
