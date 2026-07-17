87b3eb8f7a3010d7ace013e9ac1e74321b618dd5
Wzy-AI-2025 <1077714114@qq.com>
2026-07-17 16:41:31 +0800

    推消息,拓扑排序最后剩下环

---
+++ C_LeetCode_AlgorithmWorld/图论/拓扑排序/2127_Maximum_Employees_to_Be_Invited.c
@@ -0,0 +1,61 @@
+int maximumInvitations(int* favorite, int favoriteSize) {
+    int n = favoriteSize;
+    // 统计入度，便于进行拓扑排序
+    int indeg[n];
+    memset(indeg, 0, sizeof(indeg));
+    for (int i = 0; i < n; ++i) {
+        ++indeg[favorite[i]];
+    }
+
+    int used[n], f[n];
+    memset(used, 0, sizeof(used));
+    for (int i = 0; i < n; i++) {
+        f[i] = 1;
+    }
+    
+    int q[n];
+    int head = 0, tail = 0;
+    for (int i = 0; i < n; ++i) {
+        if (!indeg[i]) {
+            q[tail++] = i;
+        }
+    }
+    while (head != tail) {
+        int u = q[head++];
+        used[u] = true;
+        int v = favorite[u];
+        // 状态转移
+        f[v] = fmax(f[v], f[u] + 1);
+        --indeg[v];
+        if (!indeg[v]) {
+            q[tail++] = v;
+        }
+    }
+    // ring 表示最大的环的大小
+    // total 表示所有环大小为 2 的「基环内向树」上的最长的「双向游走」路径之和
+    int ring = 0, total = 0;
+    for (int i = 0; i < n; ++i) {
+        if (!used[i]) {
+            int j = favorite[i];
+            // favorite[favorite[i]] = i 说明环的大小为 2
+            if (favorite[j] == i) {
+                total += f[i] + f[j];
+                used[i] = used[j] = true;
+            }
+            // 否则环的大小至少为 3，我们需要找出环
+            else {
+                int u = i, cnt = 0;
+                while (true) {
+                    ++cnt;
+                    u = favorite[u];
+                    used[u] = true;
+                    if (u == i) {
+                        break;
+                    }
+                }
+                ring = fmax(ring, cnt);
+            }
+        }
+    }
+    return fmax(ring, total);
+}
